5bbb056ae70ae52360a0f905bc2e7320c4cc98b8
owbird <owbirdphorccuo@gmail.com>
2024-07-21 13:44:06 +0000

    chore (api): cache on successful requests

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -9,7 +9,17 @@ import { cacheTime } from "../../utils/constants.js";
 const userRouter = express.Router();
 const { middleware: cache } = apicache;
 
-userRouter.get("/stat", cache(cacheTime.API_CACHE_TIME), getUserStats);
-userRouter.get("/leaderboard", cache(cacheTime.API_CACHE_TIME), getLeaderboard);
+const onlyStatus200 = (_, res) => res.statusCode === 200;
+
+userRouter.get(
+  "/stat",
+  cache(cacheTime.API_CACHE_TIME, onlyStatus200),
+  getUserStats,
+);
+userRouter.get(
+  "/leaderboard",
+  cache(cacheTime.API_CACHE_TIME, onlyStatus200),
+  getLeaderboard,
+);
 
 export default userRouter;
